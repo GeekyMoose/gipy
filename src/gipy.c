c205f020332cb008403198e0a0899f2b3bce676b
geekymoose <constantin.masson@etu.univ-paris-diderot.fr>
2015-12-01 03:31:23 +0100

    Add working debug in gipy.c

--- src/gipy.c
+++ src/gipy.c
@@ -9,6 +9,7 @@
  */
 
 #include "gipy.h"
+#include "errman.h"
 
 
 //*****************************************************************************
@@ -17,15 +18,15 @@
 /**
  * @brief		Enable a GPIO Pin.
  *
- * @param		int the pin int number value
+ * @param pPin	int the pin int number value
  * @return		1 if successfully enabled otherwise, return -1
  */
 int GIPY_pinEnable(int pPin){
-	dbgMessage(("Try to enable pin %d", pPin));
+	dbgMessage((DBG_PARAMS, "Try to enable pin %d", pPin));
 
 	//Check if pin is valid
 	if(isValidPinNumber(pPin)==FALSE){
-		dbgError(("Invalid pin number: %d", pPin));
+		dbgError((DBG_PARAMS, "Invalid pin number: %d", pPin));
 		printError("Unable to enable the pin %d: invalid pin number", pPin);
 		return -1;
 	}
@@ -33,7 +34,7 @@ int GIPY_pinEnable(int pPin){
 	//Open the export sys file, check if successfully opened
 	int file = open(GPIO_PATH"/export", O_WRONLY);
 	if(file == -1){
-		dbgErrorr(("Unreachable export file: "GPIO_PATH"/export"));
+		dbgError((DBG_PARAMS, "Unreachable export file: "GPIO_PATH"/export"));
 		printError("Unable to enable the pin %d", pPin);
 		return -1;
 	}
@@ -42,7 +43,7 @@ int GIPY_pinEnable(int pPin){
 	char tamp[3];
 	sprintf(tamp, "%d", pPin);
 	if(write(file, tamp, 3) != 3){
-		dbgError(("Unable to write %d in "GPIO_PATH"/export", pPin));
+		dbgError((DBG_PARAMS, "Unable to write %d in "GPIO_PATH"/export", pPin));
 		printError("Unable to enable the pin %d", pPin);
 		close(file);
 		return -1;
@@ -58,19 +59,19 @@ int GIPY_pinEnable(int pPin){
  * @return		1 if successfully disabled, otherwise, return -1
  */
 int GIPY_pinDisable(int pPin){
-	dbgMessage(("Try to disable pin %d", pPin));
+	dbgMessage((DBG_PARAMS, "Try to disable pin %d", pPin));
 
 	//Check whether pin number is valid
 	if(isValidPinNumber(pPin)==FALSE){
-		dbgError((BDG_PARAMS, "Invalid pin number: %d", pPin));
+		dbgError((DBG_PARAMS, "Invalid pin number: %d", pPin));
 		printError("Unable to disable pin %d! Invalid pin", pPin);
 		return -1;
 	}
 
 	//Try to open unexport file
-	int file = open(GPIO_PATH"/unexport", WRONLY);
+	int file = open(GPIO_PATH"/unexport", O_WRONLY);
 	if(file<0){
-		dbgError(("Unreachable file: %s", GPIO_PATH"/unexport"));
+		dbgError((DBG_PARAMS, "Unreachable file: %s", GPIO_PATH"/unexport"));
 		printError("Unable to disable the pin %d", pPin);
 		return -1;
 	}
@@ -79,7 +80,7 @@ int GIPY_pinDisable(int pPin){
 	char tamp[3];
 	sprintf(tamp, "%d", pPin);
 	if(write(file, tamp, 3) != 3){
-		dbgError(("Unable to write %d in "GPIO_PATH"/unexport", pPin));
+		dbgError((DBG_PARAMS, "Unable to write %d in "GPIO_PATH"/unexport", pPin));
 		printError("Unable to disable the pin %d", pPin);
 		close(file);
 		return -1;
@@ -95,16 +96,16 @@ int GIPY_pinDisable(int pPin){
 /**
  * @brief	Read the value from a specific pin
  *
- * @param	int the pin number where to read
- * @return	an int corresponding to the pin state (0 or 1)
+ * @param pPin	int the pin number where to read
+ * @return		Int value corresponding to the pin state (0 or 1)
  */
 int GIPY_pinRead(int pPin){
-	dbgMessage((DBG_PARAMS, "Try to read pin %d", pPIn));
+	dbgMessage((DBG_PARAMS, "Try to read pin %d", pPin));
 
 	//Check if pin is valid
 	if(isValidPinNumber(pPin)==FALSE){
-		dbgError((DBG_PARAMS, "Invalid pin number: %d", pPin)); //Deleted if debug not set
-		printError("Invalid pin number: %d", pPin);
+		dbgError((DBG_PARAMS, "Invalid pin number: %d", pPin));
+		printError("Unable to read from pin %d! Invalid pin", pPin);
 		return -1;
 	}
 
@@ -114,7 +115,7 @@ int GIPY_pinRead(int pPin){
 	int file = open(stamp, O_RDONLY);
 	if(file < 1){
 		dbgError((DBG_PARAMS, "Unable to open the file: %s", stamp));
-		printError("Unable to open the file: %s", stamp);
+		printError("Unable to read from pin %d", pPin);
 		return -1;
 	}
 	
@@ -124,7 +125,7 @@ int GIPY_pinRead(int pPin){
 	if((n = read(file, &buff, 1)) == -1){
 		close(file);
 		dbgError((DBG_PARAMS, "Unable to read from file: %s", stamp));
-		printError("Unable to read from file: %s", stamp);
+		printError("Unable to read from pin %d", pPin);
 		return -1;
 	}
 	close(file);
@@ -136,17 +137,17 @@ int GIPY_pinRead(int pPin){
  * @detail	The pin must have been enabled before. If the value given is not 
  * 			valid, the process is stopped and return the error state
  *
- * @param	Pin number where to write
- * @param	Value to set to this pin (should be from pinValue enum)
- * @return	1 if successfully written, otherwise, return -1
+ * @param pPin		Pin number where to write
+ * @param pValue	Value to set to this pin (should be from pinValue enum)
+ * @return			1 if successfully written, otherwise, return -1
  */
 int GIPY_pinWrite(int pPin, pinValue pValue){
-	dbgMessage((DBG_PARAMS, "Try to write in pin %d", pPIn));
+	dbgMessage((DBG_PARAMS, "Try to write in pin %d", pPin));
 
 	//Check if pin is valid
 	if(isValidPinNumber(pPin)==FALSE){
 		dbgError((DBG_PARAMS, "Invalid pin number: %d", pPin));
-		printError("Invalid pin number: %d", pPin);
+		printError("Unable to write in %d! Invalid pin!", pPin);
 		return -1;
 	}
 
@@ -162,7 +163,8 @@ int GIPY_pinWrite(int pPin, pinValue pValue){
 	sprintf(stamp, GPIO_PATH"gpio%d/value", pPin);
 	int file = open(stamp, O_WRONLY);
 	if(file < 1){
-		printError("Unable to open the file: %s", stamp);
+		dbgError((DBG_PARAMS, "Unable to open the file: %s", stamp));
+		printError("Unable to write in pin %d", pPin);
 		return -1;
 	}
 
@@ -170,8 +172,9 @@ int GIPY_pinWrite(int pPin, pinValue pValue){
 	char buff[2];
 	sprintf(buff, "%d", pValue);
 	if(write(file, buff, 2) != 2){
-		printError("Unable to write the value %d in file %s", pValue, stamp);
 		close(file);
+		dbgError((DBG_PARAMS, "Unable to write the value %d in file %s", pValue, stamp));
+		printError("Unable to write in pin %d", pPin);
 		return -1;
 	}
 	close(file);
